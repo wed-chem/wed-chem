19a7a69fb8d9b74e72e1c7c059f26756edbddd34
Daniel Mikel <daniel@Daniels-MacBook-Air-2.local>
2026-03-01 20:20:56 -1000

    restore subtle fade-in transition without freeze risk

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <NavBar />
   <main id="main-content">
-    <router-view :key="$route.fullPath" />
+    <router-view v-slot="{ Component }">
+      <transition name="fade">
+        <component :is="Component" :key="$route.fullPath" />
+      </transition>
+    </router-view>
   </main>
   <FooterSection />
 </template>
@@ -11,4 +15,7 @@ import NavBar from '@/components/layout/NavBar.vue'
 import FooterSection from '@/components/layout/FooterSection.vue'
 </script>
 
-
+<style>
+.fade-enter-active { transition: opacity 0.25s ease; }
+.fade-enter-from { opacity: 0; }
+</style>
